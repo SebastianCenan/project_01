d7f4f9c336a7bcb6c9f47b85d6d5a340b49f8489
Cenan Sebastian <sebi.cenan12@gmail.com>
2020-09-03 22:01:56 +0300

    Initial commit

---
+++ lib/day_01.dart
@@ -0,0 +1,12 @@
+void main(){
+  
+
+}
+
+
+
+//Tema1
+//TODO: Defineste 2 variabile de tip int si adauga o valoare si printeaza valoarea variabilei cu functia| print(); |. Dupa ce le-ai afisat in consola, aduna variabilele, comparale,inmultestele.
+//poti incerca la fel dupa cu double
+//Tema 2
+//TODO: Defineste 2 variabile cu numele tau si a lui rebe. Verifica cate litere contine fiecare nume/variabila si compara daca sunt egale. daca sunt egale printeaza ,,true'' daca nu sunt egale printeaza ,,false''
