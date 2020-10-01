bd2d5125e341e7a754453f3f6a526fd5a6a6ccec
Cenan Sebastian <sebi.cenan12@gmail.com>
2020-10-01 21:22:09 +0300

    test

--- lib/day_01.dart
+++ lib/day_01.dart
@@ -1,10 +1,23 @@
-void main(){
-  
-
+void main() {
+  var sot = 'sebi';
+  String sotie = 'rebeca';
+  print('sotul: $sot este itist');
+  adunare();
+}
+void adunare() {
+  var mercedes = 2;
+  int opel = 4;
+  print ( mercedes * opel );
 }
 
+printulfunctiei(){
 
-
+  print(containerHeight2);
+}
+var x = 6;
+var y = 10;
+var z = 15;
+var containerHeight2 = 105435430;
 //Tema1
 //TODO: Defineste 2 variabile de tip int si adauga o valoare si printeaza valoarea variabilei cu functia| print(); |. Dupa ce le-ai afisat in consola, aduna variabilele, comparale,inmultestele.
 //poti incerca la fel dupa cu double
---
+++ lib/day_02.dart
@@ -0,0 +1,18 @@
+void main() {
+  var mercedes = 20000;
+  var x6 = 17000;
+  var catiBaniAm =300;
+
+  if (catiBaniAm == x6) {
+    print(' ne luam x6');
+  } else if (catiBaniAm == mercedes) {
+    print('isi ia tata mertan');
+  } else
+
+
+    {
+    print('object');
+  }
+}
+
+//TODO: Declara o variabila(catiBaniAm = o suma de bani). declara 14variabile de tip int cu preturi la diferite modele. Verifica daca catiBaniAm este egal cu vreuna din variabile si printeaza modelul si anul. daca nu ai bani, printeaza, mai astept 1 an.
